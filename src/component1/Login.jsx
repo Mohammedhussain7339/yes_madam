e3baa76d979b245414d311cc5f704e3de43cf91d
riyanrathore26 <rk5098863@gmail.com>
2024-04-25 23:34:33 +0530

    login processing

--- src/component1/Login.jsx
+++ src/component1/Login.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>
-      Login
+    <>
+    <div className='login-page'>
+      <h2>MY ACCOUNT</h2>
+      <div className="sublog-page">
+      <div className="login-box">
+        <h2 >Login </h2><br />
+        <h4>If you have an Account with us. please login.</h4><br />
+        <input type="text" placeholder='Email' /><br /><br />
+        <input type="text" placeholder='Password' /><br /><br />
+        <input type="submit" className='lsubmit' style={{fontSize:'15px'}}/>
+      </div> 
+      <div className="login-box">
+        <h2>NEW CUSTOMER? </h2>
+        <div style={{ fontSize:'15px',margin:'15px 0'}}>Registering for this site history. We'll get a new account<br/><br /> set up for your in no time. For this will only ask yor for<br/><br /> information necessary to make the puchase process faster and easier
+</div><br />
+      <div className="lrsubmit"><a href="/Signup">CREATE AN ACCOUNT</a></div>
+      </div> 
+      </div>
     </div>
+    </>
   )
 }
 
--- src/component1/Signup.jsx
+++ src/component1/Signup.jsx
@@ -1,10 +1,78 @@
 import React from 'react'
+import { useState,useEffect } from 'react';
+import axios from 'axios';
 
 function Signup() {
+  const [userInfo, setUserInfo] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+    contact:'',
+
+  });
+
+  const changeHandler=(e)=>{
+    const { name, value } = e.target;
+    setUserInfo({ ...userInfo, [name]: value });
+    console.log(userInfo);
+
+  }
+  const submitHandler=async(e)=>{
+    e.preventDefault();
+    console.log("Facility Name:", userInfo);
+
+    const formData = new FormData();
+    formData.append("firstname", userInfo.firstname);
+    formData.append("lastname", userInfo.lastname);
+    formData.append("age", userInfo.age);
+    formData.append("gender", userInfo.gender);
+    formData.append("email", userInfo.email);
+    formData.append("password", userInfo.password);
+    formData.append("contact", userInfo.contact);
+    // console.log(firstname);
+
+    try {
+      const res = await axios.post("http://localhost:5000/api/register", formData);
+  
+      console.log("Response:", res); // Log the entire response object
+  
+      if (res && res.data) {
+        console.log("Response Data:", res.data); // Log the response data if available
+      } else {
+        console.log("No response data received.");
+      }
+  
+      // Optionally, reset the form fields or handle success message/display here
+    } catch (err) {
+      console.error("Error:", err);
+      // Handle error, display error message, etc.
+    }
+  
+
+  }
   return (
-    <div>
-      signup
+    <>
+    <div className='register-page'>
+      <div className="register-box">
+        <h2 style={{textAlign:'center'}}>CREATE AN ACCOUNT</h2>
+        <input type="text" onChange={changeHandler} placeholder='First Name' className='name' value={userInfo.firstname} name='firstname' />
+        <input type="text" onChange={changeHandler} placeholder='Last Name ' className='name'value={userInfo.lastname} name='lastname' />
+        <input type="text" onChange={changeHandler} placeholder='Age ' className='name' value={userInfo.age} name='age' />
+        <select  id="" className='name1' onChange={changeHandler} value={userInfo.gender} name='gender'>
+          <option value="" disabled>Select Your Gender</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+          <option value="Other">Other</option>
+          </select>        
+        <input type="text" onChange={changeHandler} placeholder='Email' value={userInfo.email} name='email' />
+        <input type="text" onChange={changeHandler} placeholder='Password' value={userInfo.password} name='password' />
+        <input type="text" onChange={changeHandler} placeholder='Contact Number' value={userInfo.contact} name='contact' />
+        
+        <input type="submit" onClick={submitHandler} className='rsubmit' />
+      </div>
     </div>
+    </>
   )
 }
 
